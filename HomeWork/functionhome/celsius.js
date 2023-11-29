29a9ea96a120eb7d35f614fad1514d52742e24f9
thinnakon <thinnakon.ph@cs.rmu.ac.th>
2023-11-29 23:44:11 +0700

    Up dat

---
+++ HomeWork/functionhome/celsius.js
@@ -0,0 +1,10 @@
+function convertFahrenheitToCelsius(fahrenheit) {
+    // คำนวณอุณหภูมิในหน่วย Celsius
+    let celsius = (fahrenheit - 32) * 5/9;
+    return celsius;
+}
+// ตัวอย่างการเรียกใช้ฟังก์ชัน
+let fahrenheitTemperature = parseFloat(prompt('Enter the temperature in Fahrenheit:'));
+let celsiusTemperature = convertFahrenheitToCelsius(fahrenheitTemperature);
+
+console.log(`${fahrenheitTemperature} Fahrenheit is equal to ${celsiusTemperature.toFixed(2)} Celsius.`);
---
+++ HomeWork/functionhome/factorial.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+let number = parseInt(prompt('Enter a number:'));
+let result = factorial(number);
+console.log(`Factorial of ${number} is: ${result}`);
